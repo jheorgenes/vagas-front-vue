545abee3aae9b29e4effe734d797071382192461
Jheorgenes Warlley <jheorgenes@gmail.com>
2023-06-22 17:54:31 -0300

    Implementando os componentes: Home e PesquisarVaga

--- src/components/views/Home.vue
+++ src/components/views/Home.vue
@@ -1,12 +1,41 @@
 <template>
-  <div>
-    <h1>Componente Home</h1>
+  <div class="container py-4">
+    <div class="row">
+      <div class="col">
+        <pesquisar-vaga></pesquisar-vaga>
+      </div>
+    </div>
+    <div class="row mt-5">
+      <div class="col-4">
+        <div class="h-100 p-5 rounded-3 border bg-dark text-white">
+          <p>Vagas abertas</p>
+          <h2>25</h2>
+        </div>
+      </div>
+      <div class="col-4">
+        <div class="h-100 p-5 rounded-3 border bg-dark text-white">
+          <p>Profissionais cadastrados</p>
+          <h2>125</h2>
+        </div>
+      </div>
+      <div class="col-4">
+        <div class="h-100 p-5 rounded-3 border bg-light text-dark">
+          <p>Visitantes Online</p>
+          <h2>15</h2>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import PesquisarVaga from '@/components/comuns/PesquisarVaga.vue';
+
 export default {
   name: 'Home',
+  components: {
+    PesquisarVaga
+  },
   created() {
     console.log('Criado -', this.teste);
   },
