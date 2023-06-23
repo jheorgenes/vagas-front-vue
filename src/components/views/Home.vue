65a2f8a55065d7f4e47fa40c57a5e19a47eed40b
Jheorgenes Warlley <jheorgenes@gmail.com>
2023-06-23 11:23:32 -0300

    Tipagem de Props

--- src/components/views/Home.vue
+++ src/components/views/Home.vue
@@ -5,6 +5,18 @@
         <pesquisar-vaga></pesquisar-vaga>
       </div>
     </div>
+    <div class="row mt-5" v-for="(vaga, index) in vagas" :key="index">
+      <div class="col">
+        <vaga 
+          :titulo="vaga.titulo"
+          :descricao="vaga.descricao"
+          :salario="vaga.salario"
+          :modalidade="vaga.modalidade"
+          :tipo="vaga.tipo"
+          :publicacao="vaga.publicacao"
+        />
+      </div>
+    </div>
     <div class="row mt-5">
       <div class="col-4">
         <indicador titulo="Vagas abertas" indicador="100" bg="bg-dark" color="text-white"></indicador>
@@ -13,7 +25,7 @@
         <indicador titulo="Profissionais cadastrados" indicador="225" bg="bg-dark" color="text-white"></indicador>
       </div>
       <div class="col-4">
-        <indicador titulo="Visitantes online" indicador="25" bg="bg-light" color="text-dark"></indicador>
+        <indicador titulo="Visitantes online" :indicador="usuariosOnline" bg="bg-light" color="text-dark"></indicador>
       </div>
     </div>
   </div>
@@ -22,27 +34,67 @@
 <script>
 import PesquisarVaga from '@/components/comuns/PesquisarVaga.vue';
 import Indicador from '@/components/comuns/Indicador.vue'
+import Vaga from '@/components/comuns/Vaga.vue'
 
 export default {
   name: 'Home',
   components: {
     PesquisarVaga,
-    Indicador
+    Indicador,
+    Vaga
+  },
+  data: () => ({
+    usuariosOnline: 0,
+    vagas: [
+      {
+        titulo: 'Analista Programador PHP Pleno',
+        descricao: 'Profissional com conhecimentos em PHP, Laravel e MySQL. Necessário 3 anos de experiências. Atuará na manutenção de sistemas legados da empresa.',
+        salario: '6000',
+        modalidade: 'Home Office',
+        tipo: 'PJ',
+        publicacao: '2021-10-10'
+      },
+      {
+        titulo: 'Programador JavaScript Angular',
+        descricao: 'Profissional com conhecimentos avançados em JavaScript e Angular.',
+        salario: 5000,
+        modalidade: 'Presencial',
+        tipo: 'CLT',
+        publicacao: '2021-10-07'
+      },
+      {
+        titulo: 'Programador JavaScript Vue',
+        descricao: 'Profissional com conhecimentos avançados em JavaScript e Vue.',
+        salario: 5000,
+        modalidade: 'Home Office',
+        tipo: 'CLT',
+        publicacao: '2021-10-06'
+      },
+      {
+        titulo: 'Analista de Banco de Dados Sênior',
+        descricao: 'Domínio dos bancos de dados SQL Server, Oracle, Postgre e MySQL',
+        salario: 9000,
+        modalidade: 'Presencial',
+        tipo: 'PJ',
+        publicacao: '2021-10-06'
+      },
+      {
+        titulo: 'Programador Web Júnior',
+        descricao: 'Conhecimentos básicos em HTML, CSS, JavaScript, Bootstrap, PHP e MySQL',
+        salario: 3000,
+        modalidade: 'Presencial',
+        tipo: 'CLT',
+        publicacao: '2021-10-05'
+      }
+    ]
+  }),
+  methods: {
+    getUsuariosOnline() {
+      this.usuariosOnline = Math.floor(Math.random() * 101) //Randômico entre 0 e 100
+    }
   },
   created() {
-    console.log('Criado -', this.teste);
-  },
-  activated(){
-    console.log('Componente é ativado');
-  },
-  deactivated() {
-    console.log('Componente é desativado');
-  },
-  beforeUnmount() {
-    console.log('Antes de desmontar/destruir');
-  },
-  unmounted() {
-    console.log('Desmontado/destruído');
+    setInterval(this.getUsuariosOnline, 1000) //a cada 1 segundo essa função será executada
   }
 }
 </script>
