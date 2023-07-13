e1a1626fea2acead629f98296516c0bad9b9bb0b
Jheorgenes Warlley <jheorgenes@gmail.com>
2023-07-13 14:14:56 -0300

    - Implementando recurso de pesquisa de Vagas

--- src/components/comuns/PesquisarVaga.vue
+++ src/components/comuns/PesquisarVaga.vue
@@ -5,20 +5,28 @@
       <div class="col">
         <div class="form-group">
           <label for="">Titulo da vaga</label>
-          <input type="text" class="form-control" placeholder="Pesquise por palavras chaves, por exemplo 'PHP', 'Pleno', 'Analista'">
+          <input type="text" class="form-control" placeholder="Pesquise por palavras chaves, por exemplo 'PHP', 'Pleno', 'Analista'" v-model="titulo">
           <small class="form-text text-muted">Informe palavras que estejam relacionadas com o título da vaga que você procura</small>
         </div>
       </div>
     </div>
     <div class="row">
       <div class="col">
-        <button class="btn btn-outline-dark mt-2" type="button">Buscar</button>
+        <button class="btn btn-outline-dark mt-2" type="button" @click="pesquisarVaga()">Buscar</button>
       </div>
     </div>
   </div>
 </template>
 <script>
 export default {
-  name: 'PesquisarVaga'
+  name: 'PesquisarVaga',
+  data: () => ({
+    titulo: ''
+  }),
+  methods: {
+    pesquisarVaga(){
+      this.emitter.emit('filtrarVagas', { titulo: this.titulo });
+    },
+  }
 }
 </script>
